525ca518b3b1d3345fe0738ac702f741df6e1c45
Kuba <jakubmalinowski378@gmail.com>
2024-12-05 16:47:18 +0100

    Day 5 added

---
+++ Day5 Part2/Program.cs
@@ -0,0 +1,39 @@
+﻿using var streamReader = new StreamReader("input.txt");
+string line;
+var pageOrderingRules = new Dictionary<int, HashSet<int>>();
+while ((line = streamReader.ReadLine()!) != string.Empty)
+{
+    var splittedValues = line.Split('|').Select(int.Parse).ToArray();
+    if (pageOrderingRules.TryGetValue(splittedValues[0], out var hashset))
+        hashset.Add(splittedValues[1]);
+    else
+        pageOrderingRules[splittedValues[0]] = [splittedValues[1]];
+}
+var updates = new List<int[]>();
+while ((line = streamReader.ReadLine()!) != null)
+{
+    updates.Add(line.Split(',').Select(int.Parse).ToArray());
+}
+int sum = 0;
+foreach (var update in updates)
+{
+    bool hasBeenFixed = false;
+    int n = update.Length;
+    for (int i = 1; i < n; i++)
+    {
+        var isInPageOrdering = pageOrderingRules.TryGetValue(update[i], out var cantAppearBefore);
+        if (!isInPageOrdering)
+            continue;
+        var intersection = update[..i].Intersect(cantAppearBefore!).ToArray();
+        if (intersection.Length > 0)
+        {
+            hasBeenFixed = true;
+            int j = Array.IndexOf(update, intersection[0]);
+            (update[i], update[j]) = (update[j], update[i]);
+            i = j - 1;
+        }
+    }
+    if (hasBeenFixed)
+        sum += update[n / 2];
+}
+Console.WriteLine(sum);
---
+++ Day5/Program.cs
@@ -0,0 +1,36 @@
+﻿using var streamReader = new StreamReader("input.txt");
+string line;
+var pageOrderingRules = new Dictionary<int, HashSet<int>>();
+while ((line = streamReader.ReadLine()!) != string.Empty)
+{
+    var splittedValues = line.Split('|').Select(int.Parse).ToArray();
+    if (pageOrderingRules.TryGetValue(splittedValues[0], out var hashset))
+        hashset.Add(splittedValues[1]);
+    else
+        pageOrderingRules[splittedValues[0]] = [splittedValues[1]];
+}
+var updates = new List<int[]>();
+while ((line = streamReader.ReadLine()!) != null)
+{
+    updates.Add(line.Split(',').Select(int.Parse).ToArray());
+}
+int sum = 0;
+foreach (var update in updates)
+{
+    bool isCorrect = true;
+    int n = update.Length;
+    for (int i = 1; i < n; i++)
+    {
+        var isInPageOrdering = pageOrderingRules.TryGetValue(update[i], out var cantAppearBefore);
+        if (!isInPageOrdering)
+            continue;
+        if (update[..i].Intersect(cantAppearBefore!).Any())
+        {
+            isCorrect = false;
+            break;
+        }
+    }
+    if (isCorrect)
+        sum += update[n / 2];
+}
+Console.WriteLine(sum);
