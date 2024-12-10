ab2ae89de09bbac1b518965e808c5853c7b97bf5
Kuba <jakubmalinowski378@gmail.com>
2024-12-10 20:49:00 +0100

    Day10

---
+++ Day10 Part2/Program.cs
@@ -0,0 +1,56 @@
+﻿using var streamReader = new StreamReader("input.txt");
+var map = streamReader.ReadToEnd().Split("\r\n").ToArray();
+int trailHeadRating = 0;
+int n = map.Length;
+int m = map[0].Length;
+var adjacencyList = CreateAdjacencyArray();
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        if (map[i][j] == '0')
+        {
+            trailHeadRating += Dfs((i, j), []);
+        }
+    }
+}
+Console.WriteLine(trailHeadRating);
+
+List<(int, int)>[,] CreateAdjacencyArray()
+{
+    var adjacencyArray = new List<(int, int)>[n, m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            adjacencyArray[i, j] = [];
+            if (i - 1 >= 0 && map[i - 1][j] - 1 == map[i][j])
+                adjacencyArray[i, j].Add((i - 1, j));
+            if (i + 1 < n && map[i + 1][j] - 1 == map[i][j])
+                adjacencyArray[i, j].Add((i + 1, j));
+            if (j - 1 >= 0 && map[i][j - 1] - 1 == map[i][j])
+                adjacencyArray[i, j].Add((i, j - 1));
+            if (j + 1 < m && map[i][j + 1] - 1 == map[i][j])
+                adjacencyArray[i, j].Add((i, j + 1));
+        }
+    }
+    return adjacencyArray;
+}
+
+int Dfs((int i, int j) current, HashSet<(int i, int j)> visited)
+{
+    if (map[current.i][current.j] == '9')
+        return 1;
+
+    visited.Add(current);
+    int pathCount = 0;
+    foreach (var neighbour in adjacencyList[current.i, current.j])
+    {
+        if (!visited.Contains(neighbour))
+            pathCount += Dfs(neighbour, visited);
+    }
+    visited.Remove(current);
+
+    return pathCount;
+}
---
+++ Day10/Program.cs
@@ -0,0 +1,59 @@
+﻿using var streamReader = new StreamReader("input.txt");
+var map = streamReader.ReadToEnd().Split("\r\n").ToArray();
+int trailHeadCounter = 0;
+int n = map.Length;
+int m = map[0].Length;
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        if (map[i][j] == '0')
+            trailHeadCounter += FindTrailHeadsCount(i, j);
+    }
+}
+Console.WriteLine(trailHeadCounter);
+
+int FindTrailHeadsCount(int i, int j)
+{
+    int counter = 0;
+    var stack = new Stack<(int a, int b)>();
+    stack.Push((i, j));
+    var visited = new HashSet<(int a, int b)>() { (i, j) };
+    int currentValue = '0';
+    while (stack.Count != 0)
+    {
+        var (a, b) = stack.Pop();
+        if (map[a][b] == '9')
+        {
+            counter++;
+            continue;
+        }
+        if (map[a][b] != currentValue)
+            currentValue = map[a][b];
+        //right
+        if (b + 1 < m && !visited.Contains((a, b + 1)) && map[a][b + 1] == currentValue + 1)
+        {
+            visited.Add((a, b + 1));
+            stack.Push((a, b + 1));
+        }
+        //left
+        if (b - 1 >= 0 && !visited.Contains((a, b - 1)) && map[a][b - 1] == currentValue + 1)
+        {
+            visited.Add((a, b - 1));
+            stack.Push((a, b - 1));
+        }
+        //up
+        if (a - 1 >= 0 && !visited.Contains((a - 1, b)) && map[a - 1][b] == currentValue + 1)
+        {
+            visited.Add((a - 1, b));
+            stack.Push((a - 1, b));
+        }
+        //down
+        if (a + 1 < n && !visited.Contains((a + 1, b)) && map[a + 1][b] == currentValue + 1)
+        {
+            visited.Add((a + 1, b));
+            stack.Push((a + 1, b));
+        }
+    }
+    return counter;
+}
