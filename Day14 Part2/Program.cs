bebe89276d9094f7b225ea12d04419cf6057e282
Kuba <jakubmalinowski378@gmail.com>
2024-12-14 23:40:42 +0100

    Day 14

---
+++ Day14 Part2/Program.cs
@@ -0,0 +1,52 @@
+﻿using var streamReader = new StreamReader("input.txt");
+string line;
+var robots = new List<Robot>();
+int mapHeight = 103, mapWidth = 101;
+var charArray = new char[mapHeight, mapWidth];
+while ((line = streamReader.ReadLine()!) != null)
+{
+    var data = line
+        .Replace("p=", "")
+        .Replace("v=", "")
+        .Replace(",", " ")
+        .Split(" ")
+        .Select(int.Parse)
+        .ToArray();
+    robots.Add(new Robot(data[0], data[1], data[2], data[3]));
+}
+int iterations = 0;
+while (true)
+{
+    var seen = new HashSet<(int, int)>();
+    foreach (var robot in robots)
+    {
+        robot.X = (((robot.X + robot.Dx) % mapWidth) + mapWidth) % mapWidth;
+        robot.Y = (((robot.Y + robot.Dy) % mapHeight) + mapHeight) % mapHeight;
+        seen.Add((robot.X, robot.Y));
+    }
+    iterations++;
+    if (seen.Count == robots.Count)
+    {
+        Console.WriteLine($"Iterations: {iterations}");
+        foreach (var robot in robots)
+            charArray[robot.Y, robot.X] = 'X';
+        for (int j = 0; j < mapHeight; j++)
+        {
+            for (int k = 0; k < mapWidth; k++)
+            {
+                Console.Write(charArray[j, k] == '\0' ? " " : "X");
+            }
+            Console.Write('\n');
+        }
+        break;
+    }
+}
+
+
+internal class Robot(int x, int y, int dx, int dy)
+{
+    public int X { get; set; } = x;
+    public int Y { get; set; } = y;
+    public int Dx { get; set; } = dx;
+    public int Dy { get; set; } = dy;
+}
---
+++ Day14/Program.cs
@@ -0,0 +1,41 @@
+﻿using var streamReader = new StreamReader("input.txt");
+string line;
+var robots = new List<Robot>();
+int mapHeight = 103, mapWidth = 101;
+var robotsInEachQuadrant = new int[4];
+while ((line = streamReader.ReadLine()!) != null)
+{
+    var data = line
+        .Replace("p=", "")
+        .Replace("v=", "")
+        .Replace(",", " ")
+        .Split(" ")
+        .Select(int.Parse)
+        .ToArray();
+    robots.Add(new Robot(data[0], data[1], data[2], data[3]));
+}
+foreach (var robot in robots)
+{
+    int x = (((robot.X + 100 * robot.Dx) % mapWidth) + mapWidth) % mapWidth;
+    int y = (((robot.Y + 100 * robot.Dy) % mapHeight) + mapHeight) % mapHeight;
+    if (x < mapWidth / 2 && y < mapHeight / 2)
+        robotsInEachQuadrant[0]++;
+    else if (x > mapWidth / 2 && y < mapHeight / 2)
+        robotsInEachQuadrant[1]++;
+    else if (x < mapWidth / 2 && y > mapHeight / 2)
+        robotsInEachQuadrant[2]++;
+    else if (x > mapWidth / 2 && y > mapHeight / 2)
+        robotsInEachQuadrant[3]++;
+}
+Console.WriteLine(robotsInEachQuadrant[0] *
+    robotsInEachQuadrant[1] *
+    robotsInEachQuadrant[2] *
+    robotsInEachQuadrant[3]);
+
+internal class Robot(int x, int y, int dx, int dy)
+{
+    public int X { get; set; } = x;
+    public int Y { get; set; } = y;
+    public int Dx { get; set; } = dx;
+    public int Dy { get; set; } = dy;
+}
