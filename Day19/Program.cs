3a6a599451234211729adfddfc12bcd1c997c11a
Kuba <jakubmalinowski378@gmail.com>
2024-12-20 09:28:28 +0100

    Day19 Part1

---
+++ Day19/Program.cs
@@ -0,0 +1,31 @@
+﻿using var streamReader = new StreamReader("input.txt");
+
+var availablePatterns = streamReader.ReadLine()!
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .ToHashSet();
+string line;
+_ = streamReader.ReadLine();
+var words = new List<string>();
+while ((line = streamReader.ReadLine()!) != null)
+{
+    words.Add(line);
+}
+int constructedWords = 0;
+foreach (var word in words)
+{
+    if (CanConstruct(word))
+        constructedWords++;
+}
+Console.WriteLine(constructedWords);
+
+bool CanConstruct(string word)
+{
+    if (word == string.Empty)
+        return true;
+    for (int i = 0; i < word.Length; i++)
+    {
+        if (availablePatterns.Contains(word[..(i + 1)]) && CanConstruct(word[(i + 1)..]))
+            return true;
+    }
+    return false;
+}
